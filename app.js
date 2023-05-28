15c25132c1896a92cadb0609b3f71f0a55e08bb2
Alexandre azevedo <zamiel_al@hotmail.com>
2023-05-28 06:23:01 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogs")
 console.log("Apricação esta se conectando ao banco de dados")
+console.log("o paulo fez esse comit sem voce ver na aula")
+console.log("Enviando para o GitHub")
