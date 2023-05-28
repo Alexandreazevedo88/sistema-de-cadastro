cc68f573f0c4e8fc6e28a263bbb38df79a934304
Alexandre azevedo <zamiel_al@hotmail.com>
2023-05-28 06:44:14 -0300

    voltando para estado anterior

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogs")
 console.log("Apricação esta se conectando ao banco de dados")
-console.log("o paulo fez esse comit sem voce ver na aula")
-console.log("Enviando para o GitHub")
